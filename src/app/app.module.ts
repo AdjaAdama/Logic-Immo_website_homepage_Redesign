f486942efa163cf93f2eb11f5eedb644fa5d3120
adja <adjaadama.ndiongue@unchk.edu.sn>
2025-02-21 14:03:38 +0000

    home rooting config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './components/home/home.component';
+import { routes } from './app.routes';  // Importer les routes définies dans app.routes.ts
 
 
 
 @NgModule({
-  imports: [BrowserModule, CommonModule, AppComponent],
-  declarations: []
+  imports: [BrowserModule, CommonModule, AppComponent, MatSidenavModule, MatListModule, RouterModule.forRoot(routes)],
+  declarations: [HomeComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';  
+import { MatListModule } from '@angular/material/list';  
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterOutlet, MatSidenavModule, MatListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  title = 'site-vitrine';
 }
