1ccf6862e7e1f3e4d394628ed65a0e325841c7ae
adja <adjaadama.ndiongue@unchk.edu.sn>
2025-02-21 12:46:44 +0000

    initial configs

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { AppComponent } from './app.component';
+
+
+
+@NgModule({
+  imports: [BrowserModule, CommonModule, AppComponent],
+  declarations: []
+})
+export class AppModule { }
