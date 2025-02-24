fe1bacb2e4d92e789422ca7cec795f6513b0f15e
adja <adjaadama.ndiongue@unchk.edu.sn>
2025-02-24 13:15:53 +0000

    premiere version site

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSidenavModule } from '@angular/material/sidenav';  
-import { MatListModule } from '@angular/material/list';  
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, MatListModule],
+  imports: [CommonModule, RouterModule, MatSidenavModule, MatListModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  title = 'site-vitrine';
+  properties = [
+    { title: 'Appartement à Paris', price: '750 000', image: '../../assets/depositphotos_21515189-stock-photo-agent-with-house-model-and.jpg' },
+    { title: 'Villa à Nice', price: '1 200 000', image: '../../assets/maison.jpg' },
+    { title: 'Maison à Lyon', price: '500 000', image: '../../assets/maisonminiat.jpg' }
+  ];
+
+  trackProperty(index: number, property: any): number {
+    return index;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './app/components/header/header.component';
+import { ContentComponent } from './app/components/content/content.component';
+import { FooterComponent } from './app/components/footer/footer.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes), 
+    importProvidersFrom(CommonModule, MatSidenavModule, MatListModule)  // Importation des modules Angular Material et CommonModule
+  ]
+}).catch(err => console.error(err));
