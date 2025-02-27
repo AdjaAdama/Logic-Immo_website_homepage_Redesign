541a14dd7d09b677a74bd8deb2a9cbc833ec88dd
adja <adjaadama.ndiongue@unchk.edu.sn>
2025-02-27 12:53:09 +0000

    amelioration header

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,20 +4,23 @@ import { RouterModule } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from "../footer/footer.component";
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatSidenavModule, MatListModule, HeaderComponent],
+  imports: [CommonModule, RouterModule, MatSidenavModule, MatListModule, MatIconModule, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
   properties = [
-    { title: 'Appartement à Paris', price: '750 000', image: '../../assets/depositphotos_21515189-stock-photo-agent-with-house-model-and.jpg' },
-    { title: 'Villa à Nice', price: '1 200 000', image: '../../assets/maison.jpg' },
-    { title: 'Maison à Lyon', price: '500 000', image: '../../assets/maisonminiat.jpg' }
+    { title: 'Decouvrez maisons du monde', price: '750 000', image: '../../assets/maisonsdumonde.jpg' },
+    { title: 'Realisez vos projets avec Sofinco', price: '1 200 000', image: '../../assets/sofinco2.png' },
+    { title: 'Redecouvrez votre espace de vie', price: '500 000', image: '../../assets/velux.png' }
   ];
 
   trackProperty(index: number, property: any): number {
