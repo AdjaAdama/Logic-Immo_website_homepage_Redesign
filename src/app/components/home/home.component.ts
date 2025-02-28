d004a33e3e7b299a8307ce746cdc2315b213b6cb
adja <adjaadama.ndiongue@unchk.edu.sn>
2025-02-28 11:51:57 +0000

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -18,13 +18,13 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class HomeComponent {
   properties = [
-    { title: 'Decouvrez maisons du monde', buttonLabel: 'Decouvrir', image: '../../assets/maisonsdumonde.jpg' },
-    { title: 'Realisez vos projets avec Sofinco', buttonLabel: 'Simuler voter projet', image: '../../assets/sofinco2.png' },
-    { title: 'Redecouvrez votre espace de vie', buttonLabel: 'Decouvrir', image: '../../assets/velux.png' }
+    { title: 'Decouvrez maisons du monde', description: 'Inspirer à chacun l’envie de s’ouvrir au monde, pour créer ensemble des lieux de vie uniques, chaleureux et durables.', buttonLabel: 'Decouvrir', image: '../../assets/maisonsdumonde.jpg' },
+    { title: 'Realisez vos projets avec Sofinco', description: 'Rénover votre logement, redécorer, ou aménager votre intérieur ? Sofinco vous propose des offres adaptées à vos besoins.', buttonLabel: 'Simuler voter projet', image: '../../assets/sofinco2.png' },
+    { title: 'Redecouvrez votre espace de vie', description: 'VELUX, la lumière naturelle et l’air frais au service de votre habitat', buttonLabel: 'Decouvrir', image: '../../assets/velux.png' }
   ];
 
   trackProperty(index: number, property: any): number {
-    return index;
+    return index; 
   }
 
 }
